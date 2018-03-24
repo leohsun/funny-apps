008322d685f47878e0af36afc3bbe09077996696
廖洵 <leohsun@leohsun-mac.local>
2018-03-24 11:13:04 +0800

    improve icon support

--- widgets/_Button.js
+++ widgets/_Button.js
@@ -3,50 +3,34 @@ import PropTypes from 'prop-types'
 
 import { TouchableOpacity, Text, StyleSheet, View, Image } from 'react-native'
 
+import Icon from './_Icon'
+
 export default class _Button extends React.Component {
     static propTypes = {
         _text: PropTypes.string,
         _onPress: PropTypes.func,
-        _style: PropTypes.object,
+        _style: PropTypes.object, //btn container style
         _textStyle: PropTypes.object,
-        _icon: PropTypes.oneOf(['commend', 'share', 'deny', 'comment']),
         _renderItem: PropTypes.node,
-        _iconStyle: PropTypes.object,
-        _iconBtnStyle:PropTypes.object //iconBtn(包含icon及文本)样式
+        _icon: PropTypes.string,
+        _iconSize: PropTypes.number,
+        _iconColor: PropTypes.string,
+        _iconBtnStyle: PropTypes.object //iconBtn(包含icon及文本)样式
     }
     static defaultProps = {
         _text: 'button',
         _onPress: _ => alert('this is a button'),
-        _iconStyle:{
-            width:18,
-            height:18,
-            marginRight:6
+        _iconSize: 20,
+        _iconBtnStyle: {
+            flexDirection: 'row'
         },
-        _iconBtnStyle:{
-            flexDirection:'row'
-        }
+        _iconColor: '#bac3d4'
     }
     constructor(props) {
         super(props)
     }
-    _getImage() {
-        const commendUri = '../assets/icons/commend.png'
-        const denyUri = '../assets/icons/deny.png'
-        const commentUri = '../assets/icons/tb_message.png'
-        const shareUri = '../assets/icons/share.png'
-        switch (this.props._icon) {
-            case "commend":
-                return <Image style={this.props._iconStyle} source={require(commendUri)}></Image>;
-            case "deny":
-                return <Image style={this.props._iconStyle} source={require(denyUri)}></Image>;
-            case "comment":
-                return <Image style={this.props._iconStyle} source={require(commentUri)}></Image>;
-            case "share":
-                return <Image style={this.props._iconStyle} source={require(shareUri)}></Image>;
-        }
-    }
+
     render() {
-        // const Icon = this.props._icon ? this._getImage() : null
         return (
             <TouchableOpacity
                 onPress={this.props._onPress}
@@ -54,10 +38,12 @@ export default class _Button extends React.Component {
             >
                 {this.props._renderItem ||
                     <View style={this.props._iconBtnStyle}>
-                        {this.props._icon && this._getImage()}
-                        <Text
+                        {this.props._icon && <Icon name={this.props._icon} size={this.props._iconSize} color={this.props._iconColor}></Icon>}
+                        { this.props._text!=="" &&<Text
                             style={this.props._textStyle}
-                        >{this.props._text}</Text>
+                        >
+                            {this.props._text}
+                        </Text>}
                     </View>}
 
             </TouchableOpacity>
